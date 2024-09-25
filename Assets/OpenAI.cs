cbdb6810e6f684fed28d49369607a9a6b3c8d58e
JKong05 <jkong3662@gmail.com>
2024-09-24 23:53:04 -0500

    test

--- Assets/OpenAI.cs
+++ Assets/OpenAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class verysimpletext : MonoBehaviour
+public class OpenAI : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class verysimpletext : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // my name is joshua and I love men
+        
     }
 }
