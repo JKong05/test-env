29050d2f9e8026b9e07b69180f4f25879629784b
tevinpark <tevinpark01@gmail.com>
2024-10-02 15:06:03 -0500

    playaudio merge issue solved

--- Assets/PlayAudio.cs
+++ Assets/PlayAudio.cs
@@ -3,60 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-<<<<<<< HEAD
-using System.IO;
-=======
->>>>>>> tevin
 
 public class PlayAudio : MonoBehaviour
 {
     private bool _isAudioPlaying = false;
 
-<<<<<<< HEAD
-    
-    public Button audioButton;
-    public TextMeshProUGUI buttonText;
-    private bool _isAudioPlaying = false;
-
-=======
     // Reference to the UI button, text and audio
     public Button audioButton;
     public TextMeshProUGUI buttonText;
->>>>>>> tevin
     public AudioSource soundPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
         // Set up the button listener
         audioButton.onClick.AddListener(OnButtonClick);   
     }
 
     // Handle button click
-    public void OnButtonClick()
-    {
-        if (_isAudioPlaying)
-        {
-            _isAudioPlaying = false;
-            soundPlayer.Pause();
-            buttonText.text = "Audio Paused";
-        }
-        else
-        {
-            _isAudioPlaying = true;
-            soundPlayer.Play();
-            buttonText.text = "Audio is Playing";
-        }
-    }
-
-
-=======
-        audioButton.onClick.AddListener(OnButtonClick);
-    }
-
-
-    
     public void OnButtonClick()
     {
         if (_isAudioPlaying)
@@ -72,9 +36,6 @@ public class PlayAudio : MonoBehaviour
             buttonText.text = "Audio Playing";
         }
     }
-<<<<<<< HEAD
->>>>>>> tevin
-=======
 
     public void playThisAudio()
     {
@@ -85,5 +46,4 @@ public class PlayAudio : MonoBehaviour
     {
         soundPlayer.Pause();
     }
->>>>>>> tevin
 }
