7c358bcf18e801d9977134f9b1d164dec18bf5dc
tevinpark <tevinpark01@gmail.com>
2024-10-04 18:38:55 -0500

    changed

--- Assets/HandTrackingVisualizer.cs
+++ Assets/HandTrackingVisualizer.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandTrackingVisualizer : MonoBehaviour
 {
-    public GameObject leftHandMarker;
-    public GameObject rightHandMarker;
+    public GameObject leftHandMarker;  // Reference to the left hand marker GameObject
+    public GameObject rightHandMarker; // Reference to the right hand marker GameObject
+
+    private InputDevice leftHandDevice;
+    private InputDevice rightHandDevice;
+
+    private void Start()
+    {
+        // Get the left and right hand devices
+        leftHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        rightHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+    }
 
     private void Update()
     {
-        // Update the positions of the hand markers
+        // Update the hand positions
         UpdateHandMarker(XRNode.LeftHand, leftHandMarker);
         UpdateHandMarker(XRNode.RightHand, rightHandMarker);
+
+        // Detect gestures
+        DetectGestures(leftHandDevice);
+        DetectGestures(rightHandDevice);
     }
 
     private void UpdateHandMarker(XRNode hand, GameObject marker)
@@ -18,20 +33,40 @@ public class HandTrackingVisualizer : MonoBehaviour
         InputDevice device = InputDevices.GetDeviceAtXRNode(hand);
         if (device.isValid)
         {
-            // Try to get the position and rotation of the hand
-            if (device.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position) &&
-                device.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation))
+            if (device.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position))
             {
-                // Set the marker position to the hand position
                 marker.transform.position = position;
-
-                // If you want the marker to align with the hand's rotation
-                marker.transform.rotation = rotation;
-
-                // Optionally, if you're working in a local space, you might need to adjust:
-                // Vector3 worldPosition = Camera.main.transform.TransformPoint(position);
-                // marker.transform.position = worldPosition;
             }
         }
+    }
+
+    private void DetectGestures(InputDevice handDevice)
+    {
+        if (handDevice.isValid)
+        {
+            // Example: Pinch Gesture
+            if (IsPinching(handDevice))
+            {
+                Debug.Log("Pinch Gesture Detected");
+            }
+
+            // Example: Open Hand Gesture
+            if (IsHandOpen(handDevice))
+            {
+                Debug.Log("Open Hand Gesture Detected");
+            }
+        }
+    }
+
+    private bool IsPinching(InputDevice handDevice)
+    {
+        // Check if the grip button is pressed (you can also check thumb/index finger positions if available)
+        return handDevice.TryGetFeatureValue(CommonUsages.gripButton, out bool isGripping) && isGripping;
+    }
+
+    private bool IsHandOpen(InputDevice handDevice)
+    {
+        // Check if the grip button is not pressed
+        return handDevice.TryGetFeatureValue(CommonUsages.gripButton, out bool isGripping) && !isGripping;
     }
 }
